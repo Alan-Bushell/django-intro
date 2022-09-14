47ad3c7d919f8059918273eb3f524bfedb002bbe
Alan Bushell <alanbushell91@gmail.com>
2022-09-14 16:38:17 +0000

    add for loop to templates and display items

--- todo/views.py
+++ todo/views.py
@@ -4,4 +4,8 @@ from .models import Item
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
